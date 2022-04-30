adcd3a64233723ec7ec97c67c6175f94cc11d62c
fabezio <fabezio@outlook.fr>
2022-04-30 19:30:31 +0200

    création fonction bouton

--- main.go
+++ main.go
@@ -6,25 +6,22 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+var a = app.New()
+
 func main() {
-	a := app.New()
 	win := a.NewWindow("window 1")
 	const w float32 = 1280
 	const h float32 = 800
 	win.SetContent(widget.NewLabel("Hello World!\n\nGood to see you"))
-	win.Resize(fyne.NewSize(w, h))
-	win.SetMaster()
-	win.Show()
-
-	win2 := a.NewWindow("window 2")
-	win2.SetContent(widget.NewButton("Click here", func() {
-		win3 := a.NewWindow("dependency window")
-		win3.SetContent(widget.NewLabel("What do you want to place here ?"))
-		win3.Resize(fyne.NewSize(150, 100))
-		win3.Show()
+	win.SetContent(widget.NewButton("Click here", func() {
+		btn := a.NewWindow("dependency window")
+		btn.SetContent(widget.NewLabel("Well done!"))
+		btn.Resize(fyne.NewSize(150, 100))
+		btn.Show()
 	}))
-	win2.Resize(fyne.NewSize(w/2, h/2))
-	win2.Show()
+	win.Resize(fyne.NewSize(w, h))
+	// win.SetMaster()
+	win.Show()
 
 	a.Run()
 }
