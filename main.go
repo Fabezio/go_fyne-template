1ce160cdb8c4a7fdb97a5e7f734fe1fbe9a3520e
fabezio <fabezio@outlook.fr>
2022-05-05 21:22:57 +0200

    display layouts:
    - vertical (VBox)
    - horizontal (HBox)

--- main.go
+++ main.go
@@ -1,39 +1,52 @@
 package main
 
 import (
+	"fmt"
 	colors "fyne-test/helpers"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 )
 
+var title = "fyne tutorial"
+var a = app.New()
+var win = a.NewWindow(title)
+
+var w size = 800
+var h size = 450
+
 func makeEnv() {
-	horizontalGradient := canvas.NewHorizontalGradient(colors.Orange, colors.Yellow)
-	verticalGradient := canvas.NewVerticalGradient(colors.Orange, colors.Yellow)
-	linearGradient := canvas.NewLinearGradient(colors.Orange, colors.Black, 45)
-	radialGradient := canvas.NewRadialGradient(colors.Black, colors.Orange)
-	w1.SetContent(horizontalGradient) // will display from top left to bottom right
-	w1.Show()
-	w2.SetContent(verticalGradient) // will display from top left to bottom right
-	w2.Show()
-	w3.SetContent(linearGradient) // will display from top left to bottom right
-	w3.Show()
-	w4.SetContent(radialGradient) // will display from top left to bottom right
-	w4.Show()
+	var mainDisplay *fyne.Container
+	// var secDisplay *fyne.Container
+	fmt.Printf("%T\n", mainDisplay)
+	hori := true
+	heading := canvas.NewText("This is my title", colors.Black)
+	parag := canvas.NewText("This is my paragraph", colors.Black)
+	line := canvas.NewLine(colors.Gray)
+	line.StrokeWidth = 2
+	horiBox := container.NewHBox(
+		heading,
+		line,
+		parag,
+	)
+	vertBox := container.NewVBox(
+		heading,
+		line,
+		parag,
+	)
+	if hori {
+		win.SetContent(horiBox) // will display from top left to bottom right
+
+	} else {
+		win.SetContent(vertBox) // will display from top left to bottom right
+
+	}
 
 }
 
 type size = float32
-
-var title = "a mere http url"
-var a = app.New()
-var w1 = a.NewWindow("Horizontal gradient")
-var w2 = a.NewWindow("Vertical gradient")
-var w3 = a.NewWindow("Linear gradient")
-var w4 = a.NewWindow("Radial gradient")
-var w size = 200
-var h size = 200
 
 func main() {
 	launchApp()
@@ -41,10 +54,8 @@ func main() {
 }
 
 func launchApp() {
-	w1.Resize(fyne.NewSize(w, h))
-	w2.Resize(fyne.NewSize(w, h))
-	w3.Resize(fyne.NewSize(w, h))
-	w4.Resize(fyne.NewSize(w, h))
+	win.Resize(fyne.NewSize(w, h))
+
 	makeEnv()
-	a.Run()
+	win.ShowAndRun()
 }
