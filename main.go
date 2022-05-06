62430472b2580b737777da9e80bb23957c516488
fabezio <fabezio@outlook.fr>
2022-05-06 17:14:07 +0200

    dice game: optimize

--- main.go
+++ main.go
@@ -20,30 +20,20 @@ var w size = 400
 var h size = 400
 
 func makeEnv() {
-	// heading := canvas.NewText("Random Nb Gen", colors.Black)
-	// heading.TextSize = 40
-	face1 := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-one-solid.svg"
-	face2 := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-two-solid.svg"
-	face3 := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-three-solid.svg"
-	face4 := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-four-solid.svg"
-	face5 := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-five-solid.svg"
-	face6 := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-six-solid.svg"
 	dice := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-d6-solid.svg"
-
-	faces := [6]string{face1, face2, face3, face4, face5, face6}
 
 	diceImg := canvas.NewImageFromFile(dice)
 	diceImg.FillMode = canvas.ImageFillOriginal
 
-	// img := canvas.NewImageFromFile("/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice-four-solid.svg")
 	btn := widget.NewButton("play", func() {
 		fmt.Println("generating...")
 
 		time.Sleep(250 * time.Millisecond)
 		rand := rand.Intn(6)
+		// str :=
 		fmt.Println(rand + 1)
-		diceImg.File = faces[rand]
-		diceImg.FillMode = canvas.ImageFillOriginal
+		diceImg.File = fmt.Sprintf("/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-%v-solid.svg", rand+1)
+
 		diceImg.Refresh()
 	})
 
