7e395d34581a2749b9fb65f2060414d4eff66618
fabezio <fabezio@outlook.fr>
2022-05-06 17:31:25 +0200

    display card

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
 
+	// "fyne.io/fyne/container"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
+
 	"fyne.io/fyne/v2/widget"
+	// "fyne.io/fyne/v2/theme"
 )
 
 var title = "fyne tutorial"
@@ -20,28 +19,16 @@ var w size = 400
 var h size = 400
 
 func makeEnv() {
-	dice := "/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-d6-solid.svg"
 
-	diceImg := canvas.NewImageFromFile(dice)
-	diceImg.FillMode = canvas.ImageFillOriginal
-
-	btn := widget.NewButton("play", func() {
-		fmt.Println("generating...")
-
-		time.Sleep(250 * time.Millisecond)
-		rand := rand.Intn(6)
-		// str :=
-		fmt.Println(rand + 1)
-		diceImg.File = fmt.Sprintf("/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-%v-solid.svg", rand+1)
-
-		diceImg.Refresh()
-	})
-
-	cont := container.NewVBox(
-		diceImg,
-		btn,
+	img := canvas.NewImageFromFile("/Users/fabriceriquet/Documents/dev/go/test-fyne/assets/dice/dice-4-solid.svg")
+	img.FillMode = canvas.ImageFillOriginal
+	card := widget.NewCard(
+		"title",
+		"subtitle",
+		img,
 	)
-	win.SetContent(cont)
+
+	win.SetContent(card)
 
 }
 
