e957e92f2ee4d1d61c62f5457e31f06d40953f23
fabezio <fabezio@outlook.fr>
2022-05-10 15:17:33 +0200

    password generator

--- main.go
+++ main.go
@@ -9,10 +9,16 @@ package main
 // package main
 
 import (
+	"image/color"
+	"math/rand"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 // "fyne.io/fyne/container"
@@ -30,10 +36,54 @@ var w size = 200
 var h size = 200
 
 func makeEnv() {
-	r, _ := fyne.LoadResourceFromURLString("https://avatars.githubusercontent.com/u/36045855?s=200&v=4")
+	lower := "abcdefghijklmnopqrstuvwxyz"
+	upper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numbers := "1234567890"
+	speChars := "$£@#&§!?"
+	passwdLength := 8
+	genPasswdStr := ""
+	genPasswd := canvas.NewText(genPasswdStr, color.Black)
 	// ceFromURLString("")
+	generator := widget.NewButton("generate Password", func() {
+		genPasswdStr = ""
+
+		// genPasswd.Text = ""
+		// genPasswd.Refresh()
+		for n := 0; n < passwdLength; n++ {
+			rand.Seed(time.Now().UnixNano())
+			randNum := rand.Intn(4)
+			// fmt.Println(randNum)
+			switch randNum {
+			case 0:
+				rand.Seed(time.Now().UnixNano())
+				randIndex := rand.Intn(len(lower))
+				genPasswdStr += string(lower[randIndex])
+			case 1:
+				rand.Seed(time.Now().UnixNano())
+				randIndex := rand.Intn(len(upper))
+				genPasswdStr += string(upper[randIndex])
+			case 2:
+				rand.Seed(time.Now().UnixNano())
+				randIndex := rand.Intn(len(numbers))
+				genPasswdStr += string(numbers[randIndex])
+			case 3:
+				rand.Seed(time.Now().UnixNano())
+				randIndex := rand.Intn(len(speChars))
+				genPasswdStr += string(speChars[randIndex])
+
+			}
+		}
+		genPasswd.Text = genPasswdStr
+		genPasswd.Refresh()
+	})
+
+	// fmt.Println(genPasswdStr)
+
 	// win.SetIcon(r)
-	win.SetContent(canvas.NewImageFromResource(r))
+	win.SetContent(container.NewVBox(
+		generator,
+		genPasswd,
+	))
 }
 
 type size = float32
